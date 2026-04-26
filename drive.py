db81551bf1d06d779ae22fc2dd381d91d04c561b
El_Joe <120425283+y7778@users.noreply.github.com>
2026-04-26 10:26:02 +0300

    Refactor telemetry and connection handling
    Updated telemetry function to process images and predict steering angle. Changed throttle to a fixed value and improved connection handling.

--- drive.py
+++ drive.py
@@ -7,47 +7,51 @@ import base64
 from io import BytesIO
 from PIL import Image
 import cv2
- 
+
+# إعداد السيرفر والاتصال
 sio = socketio.Server()
- 
-app = Flask(__name__) #'__main__'
-speed_limit = 10
-def img_preprocess(img):
-    img = img[60:135,:,:]
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    img = cv2.GaussianBlur(img,  (3, 3), 0)
-    img = cv2.resize(img, (200, 66))
-    img = img/255
-    return img
- 
- 
-@sio.on('telemetry')
+app = Flask(__name__)
+
+# تحميل الموديل المدرب
+model = load_model('model.h5')
+
 def telemetry(sid, data):
-    speed = float(data['speed'])
-    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    # استقبال الصورة من المحاكي
+    image = Image.open(BytesIO(base64.b64decode(data["image"])))
     image = np.asarray(image)
-    image = img_preprocess(image)
+    
+    # معالجة الصورة لتناسب الموديل (Crop & Resize)
+    image = image[60:135, :, :] 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+    image = cv2.resize(image, (200, 66))
+    image = image / 255.0 
     image = np.array([image])
-    steering_angle = float(model.predict(image))
-    throttle = 1.0 - speed/speed_limit
-    print('{} {} {}'.format(steering_angle, throttle, speed))
+
+    # توقع زاوية الدركسيون
+    steering_angle = float(model.predict(image, batch_size=1))
+    
+    # تثبيت السرعة عند 15 كم/ساعة
+    throttle = 0.15
+    
+    print(f'Steering: {steering_angle} | Throttle: {throttle}')
     send_control(steering_angle, throttle)
- 
- 
- 
-@sio.on('connect')
-def connect(sid, environ):
-    print('Connected')
-    send_control(0, 0)
- 
+
+@sio.on('telemetry')
+def on_telemetry(sid, data):
+    if data:
+        telemetry(sid, data)
+
 def send_control(steering_angle, throttle):
-    sio.emit('steer', data = {
+    sio.emit("steer", data={
         'steering_angle': steering_angle.__str__(),
         'throttle': throttle.__str__()
     })
- 
- 
+
+@sio.on('connect')
+def connect(sid, environ):
+    print("Connected to Simulator!")
+    send_control(0, 0)
+
 if __name__ == '__main__':
-    model = load_model('model/model.h5')
     app = socketio.Middleware(sio, app)
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
